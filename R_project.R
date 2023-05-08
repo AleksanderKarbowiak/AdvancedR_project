aa6e336c07b2fbf0c5203a7a3cde64f543b32f0b
AleksanderKarbowiak <63404639+AleksanderKarbowiak@users.noreply.github.com>
2023-05-08 17:18:48 +0200

    Dodanie przycisku HELP
    Wyswietla informacje o wymaganiach dotyczacych zbioru pod analize

--- R_project.R
+++ R_project.R
@@ -1,8 +1,11 @@
 library(shiny)
+library(shinyjs)
 library(readxl)
 library(ggplot2)
 
-ui <- fluidPage(
+ui <- fluidPage( 
+  useShinyjs(), 
+  actionButton("help_window", "HELP"),
   sidebarLayout(
     sidebarPanel(
       fileInput("file1", "Choose XLSX File", accept = ".xlsx"),
@@ -15,6 +18,11 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+  
+  observeEvent(input$help_window, {
+    shinyjs::runjs("var helpWindow = window.open('data:text/html,<html><body><h1>Hi, please remember that your dataset must contain these variables: ID, State, CrimeType and NumerOfCrimes.</h1><h2>Have fun :)</h2></body></html>', 'Help', 'dependent=TRUE,resizable=TRUE');helpWindow.document.title = 'Help';")
+  })
+  
   output$contents <- renderTable({
     file <- input$file1
     ext <- tools::file_ext(file$datapath)
