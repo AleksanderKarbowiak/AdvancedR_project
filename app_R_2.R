2c98795f74fec5ad6f09460604a0c9d6aee97da2
oiwanska <oliwiaiwanska98@gmail.com>
2023-05-18 01:20:39 +0200

    style + Display buttons

--- app_R_2.R
+++ app_R_2.R
@@ -1,33 +1,43 @@
-#test
 library(leaflet)
 library(shiny)
 library(shinyjs)
 library(readxl)
 library(ggplot2)
 library(readr)
+library(shinythemes)
 
 ui <- fluidPage(
-  leafletOutput('myMap', width = "100%", height = "550px"),
+  leafletOutput('myMap', width = "100%", height="100vh"),
   useShinyjs(),
-  actionButton("help_window", "HELP"),
+  theme = shinytheme("sandstone"),
   div(
     style = "position: absolute; top: 5px; right: 40px;",
     width = "300px",
+    
     fluidRow(
       fileInput("file1", "Choose CSV File", accept = ".csv"),
       checkboxInput("header", "Header", TRUE)
     ),
+    
+    radioButtons("disp", "Display",
+                 choices = c("First 100 rows" = "100_rows",
+                             "First 1000 rows" = "1000_rows",
+                             "All" = "all"),
+                 selected = "100_rows"),
+    
     fluidRow(
       selectInput("x", "X", choices = NULL),
       selectInput("y", "Y", choices = NULL)
     ),
+    
     fluidRow(
       tableOutput("output"),
       plotOutput("scatterPlot", width = "100%", height = "200px")
-    )
+    ),
+    
+    actionButton("help_window", "HELP")
   )
 )
-
 
 
 server <- function(input, output, session) {
@@ -53,12 +63,24 @@ server <- function(input, output, session) {
       data$LAT <- as.numeric(gsub("[^0-9.-]", "", data$LAT))
       data$LON <- as.numeric(gsub("[^0-9.-]", "", data$LON))
       data <- na.omit(data[c("LAT", "LON")])
-      data.frame(Lat =as.numeric(data$LAT), Lon =as.numeric(data$LON))})
-   
+      data.frame(Lat =as.numeric(data$LAT), Lon =as.numeric(data$LON))
+      
+      if(input$disp == "100_rows") {
+        return(data[1:100,])
+      }
+      else if(input$disp == "1000_rows"){
+        return(data[1:1000,])
+      }
+      else {
+        return(data)
+      }
+      
+      })
+    
     
     map <- leaflet() %>% 
       addTiles() %>% 
-      addCircles(data = data(), radius = 10, color = 'green') %>%
+      addCircles(data = data(), radius = 10, color = 'darkred') %>%
       setView(lng = -86.00, lat = 34.0000, zoom = 4)
     
     map
@@ -68,9 +90,15 @@ server <- function(input, output, session) {
     req(input$file1)
     data <- data()
     colnames(data) <- gsub(";", "", colnames(data))
-    plot(x = data[, input$x], y = data[, input$y],
-         xlab = input$x, ylab = input$y, main = paste(input$x, "vs", input$y))
-  })
+    
+    ggplot(data, aes(x=data[, input$x], y=data[, input$y])) +
+    geom_point() +
+    labs(title=paste(input$x, "vs", input$y),
+         x=input$x, y = input$y) +
+    theme(plot.background = element_rect(fill='transparent', color=NA),
+          text=element_text(face = "bold"))
+
+  },bg="transparent")
   
 
   
@@ -86,6 +114,16 @@ server <- function(input, output, session) {
     validate(need(ext == "csv", "Please upload a csv file"))
 
     read.csv(file$datapath, header = input$header)
+    
+    if(input$disp == "100_rows") {
+      return(file[1:100,])
+    }
+    else if(input$disp == "1000_rows"){
+      return(file[1:1000,])
+    }
+    else {
+      return(file)
+    }
   })
 }
 
